6f2bd2477f0ac392d1d88f43852addfaf9cc52bd
Gonçalo <goncaloaaf@gmail.com>
2025-07-08 22:20:51 +0100

    demo tipos de dados

---
+++ 01_tiposDados/main.py
@@ -0,0 +1,65 @@
+# cmt 1 linha
+
+
+"""
+cmt
+multi
+linha
+
+"""
+
+"""
+
+print("Ola Mundo")
+
+"""
+
+
+"""
+ 
+ "Txt" -> str
+ 1233 -> int
+ 41.21 -> float
+ True | False -> bool
+
+"""
+
+nome = "Gonçalo"
+# nome:str = "Gonçalo"
+
+print(nome)
+
+# nome = 1986
+
+print(nome)
+
+ano = 2026
+
+print(ano)
+
+
+print("Ola Mundo " + nome + " em " + str(ano))
+print("Ola Mundo", nome, "em",ano )
+
+print(f"Ola Mundo {nome} em {ano}")
+
+
+my_txt = "Ola Mundo" # str
+
+idade = 12 # int
+
+altura = 1.75 # float
+
+aprovado = True # bool
+
+print(f"Ola Mundo {nome} em {ano}.\nEstado {aprovado}")
+
+"""
+\n - new line
+\t - tab
+"""
+
+print("Ola\nMundo")
+
+my_txt = 10
+
