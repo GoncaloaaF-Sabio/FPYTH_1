2b89cded180b179d29b45c420f8d2b9a6b2c868f
Gonçalo <goncaloaaf@gmail.com>
2025-07-10 20:38:33 +0100

    recap

---
+++ 00_Recap/main.py
@@ -0,0 +1,23 @@
+num = input("qual a sua idade: ")
+print(type(num))
+
+num_i = int(num)
+print(type(num_i))
+
+ano_curr = 2025
+
+ano_nasc = ano_curr - num_i
+
+print(ano_nasc)
+
+
+ano_nasc2 = ano_curr - int(num)
+
+print(ano_nasc2)
+
+
+num2 = int(input("qual a sua idade: "))
+print(type(num2))
+
+cp = input("Qual o seu CP: ")
+print(type(cp))
