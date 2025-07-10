2337406e8cb4eb095e186d0c9fdb65dce742a4a0
Gonçalo <goncaloaaf@gmail.com>
2025-07-10 20:12:23 +0100

    ate if v1

---
+++ 04_if/main.py
@@ -0,0 +1,76 @@
+
+num1 = 10
+
+num2 = 20
+
+"""
+
+se num1 > num2 entao escrever "ok"
+
+"""
+#num1 = 9999
+
+"""
+    if que verifica se num1 > num2 entao escrever "ok"
+"""
+
+if num1 > num2:
+    print("ok")
+
+    print("ainda dentro do if")
+
+print("fora do if")
+
+
+"""
+
+se num1 > num2 entao escrever "ok" se nao escrever " nao ok"
+
+"""
+print("--" * 10)
+
+num1 = 20
+num2 = 20
+
+if num1 > num2:
+    print("ok")
+
+    print("ainda dentro do if")
+
+else:
+    print("nao ok")
+
+    print("ainda dentro do else")
+
+print("\n\nfora do else e do if")
+
+
+
+
+"""
+ >  --> maior 
+ <  --> menor 
+ >= --> maior ou igual
+ <= --> menor ou igual
+ == --> igual
+ != --> diferente (não igual)
+
+"""
+print("---" * 10)
+num1 = 20
+num2 = 20
+
+if num1 == num2:
+    print("São iguais")
+else:
+    print("não são iguais")
+
+
+print("---" * 10)
+num1 = 20
+num2 = 20
+
+if num1 != num2:
+    print("não São iguais")
+else:
+    print("são iguais")
