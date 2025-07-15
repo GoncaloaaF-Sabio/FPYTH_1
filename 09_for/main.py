5a2cd63bf56900f3a54b3d367a26885a39bfefdc
Gonçalo <goncaloaaf@gmail.com>
2025-07-15 21:23:05 +0100

    for v1

---
+++ 09_for/main.py
@@ -0,0 +1,14 @@
+
+# for
+
+for i in range(10): # 0 1 2 3 4 5 6 7 8 9
+    print(f"Ola Mundo -> {i}")
+
+print("----" * 5)
+
+tab = int(input("tabuada: "))
+for i in range(1, 11):
+    res = i * tab
+    print(f"{i} x {tab} = {res}")
+
+
