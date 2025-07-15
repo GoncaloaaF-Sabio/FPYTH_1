1ff1471fb7aa07061594f3919bdf85182fe62ae1
Gonçalo <goncaloaaf@gmail.com>
2025-07-15 22:02:32 +0100

    while

---
+++ 11_while/main.py
@@ -0,0 +1,21 @@
+val = 50
+
+
+count = 0
+while val >=  0:
+    count += 1 # count = count + 1
+
+    print(val)
+    # continue
+
+   # if val == 15:
+   #     break
+
+
+
+    val = val - 1 # <=> val -= 1
+
+    if val == 0:
+        print("o loop vai terminar")
+        break
+
