fbf213ea057a34957beb131a4fd4b38ae94ce507
Gonçalo <goncaloaaf@gmail.com>
2025-07-24 22:40:08 +0100

    listas ex2l

---
+++ 14_exListas/ex2.py
@@ -0,0 +1,13 @@
+"""
+
+peça ao utilizar quantos num ele quer adicionar,
+se o utilziador indicar um numero negativo deve voltar a pedir o numero de valroes a adicionar
+
+deve pedir ao utilziar a quantidade de numeros indicada.
+
+no final:
+    calcule a quantidade de valores positivos e nagativos
+    mostre o dobro de todos os num inseridos
+    calcule a quantidade de valores par e impar
+
+"""
