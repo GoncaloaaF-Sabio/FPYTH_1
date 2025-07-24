1baf8f702b756b7bdf4836d5b623fdf7f2ee7ec8
Gonçalo <goncaloaaf@gmail.com>
2025-07-24 21:05:59 +0100

    demo listas 2

--- 13_listas_v2/main.py
+++ 13_listas_v2/main.py
@@ -85,3 +85,82 @@ print(nomes.__contains__("david"))
 
 print("David" in nomes)
 print("david" in nomes)
+
+nomes.sort() # ordena
+print(nomes)
+
+nomes.sort(reverse=True)
+print(nomes)
+
+nomes.clear() # remove todos os val da lista
+print(nomes)
+
+nomes = [
+    "Ana",       #0
+    "Bruno",     #1
+    "Carla",     #2
+    "David",     #3
+    "Eduarda",   #4
+    "Fernando",  #5
+    "Gabriela",  #6
+    "Hugo",      #7
+    "Inês",      #8
+    "João"       #9
+]
+
+
+nomes = ["Beatriz", "Rafael", "Luciana", "Carlos", "Marta",
+         "Tiago", "Inês", "João", "Sofia", "Pedro"]
+
+print(nomes)
+nomes.reverse() # inverter a lista
+print(nomes)
+
+nomes.sort(reverse=True)
+print(nomes)
+
+print("---"*20)
+nomes2 = nomes
+
+print("nomes :", nomes)
+print("nomes2:", nomes2)
+
+print("---")
+nomes2[0] = "Rita"
+
+print("nomes2:", nomes2)
+print("nomes :", nomes)
+
+
+print("--copy-"*20)
+nomes3 = nomes.copy() # criar uma nova list
+
+print("nomes :", nomes)
+print("nomes3:", nomes3)
+
+print("---")
+nomes3[0] = "Joana"
+
+print("nomes3:", nomes3)
+print("nomes :", nomes)
+
+
+print("---"*20)
+
+
+nomes2 = [
+    "Ana",       #0
+    "Bruno",     #1
+    "Carla",     #2
+    "David",     #3
+    "Eduarda",   #4
+    "Fernando",  #5
+    "Gabriela",  #6
+    "Hugo",      #7
+    "Inês",      #8
+    "João"       #9
+]
+
+print(nomes)
+nomes.extend(nomes2)
+print(nomes)
