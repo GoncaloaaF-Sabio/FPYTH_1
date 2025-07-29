ce601f8dad38921e6bf2bbe6aa31dee2d121e964
GoncaloF <goncaloaaf@gmail.com>
2025-07-29 22:37:18 +0100

    ex

---
+++ 17_ex_3/main.py
@@ -0,0 +1,38 @@
+"""
+
+assuma que tem uma lista de numeros inteiros diferentes e não ordenados e um valor inteiro
+
+ache os dois numeros da lista que somados dao o valor inteiro
+
+deve indicar os numeros e as suas posições
+
+pode assumir que existe sempre uma e so uma solução
+
+exp:
+lista - [3,5,1,6]
+num - 7
+
+saida:
+
+num = [1,6]
+pos = [2, 3]
+
+"""
+
+
+
+
+"""
+
+e se a lista estver ordenada?
+
+exp:
+lista - [1,3,5,6]
+num - 7
+
+saida:
+
+num = [1,6]
+pos = [2, 3]
+
+"""
