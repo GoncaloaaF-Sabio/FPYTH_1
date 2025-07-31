d35b74959775be1215e93feb1e64ee80d4219011
Gonçalo <goncaloaaf@gmail.com>
2025-07-31 19:32:58 +0100

    old data

--- 14_exListas/ex2.py
+++ 14_exListas/ex2.py
@@ -6,8 +6,10 @@ se o utilziador indicar um numero negativo deve voltar a pedir o numero de valro
 deve pedir ao utilziar a quantidade de numeros indicada.
 
 no final:
-    calcule a quantidade de valores positivos e nagativos
-    mostre o dobro de todos os num inseridos
-    calcule a quantidade de valores par e impar
+    calcule a quantidade de valores positivos e nagativos (for)
+    mostre o dobro de todos os num inseridos (for)
+    calcule a quantidade de valores par e impar (for)
 
 """
+# 1 2 3 4
+# 2 4 6 8
