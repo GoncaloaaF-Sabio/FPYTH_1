1c7c053f4fbb0033c75b90ecb0172d8406b5dbab
GoncaloaaF <goncaloaaf@gmail.com>
2025-09-18 22:58:31 +0100

    demo classes com metodos v4

--- 24_objs_v2/ex1.py
+++ 24_objs_v2/ex1.py
@@ -5,3 +5,31 @@ crie a Classe Bola: Crie uma classe que modele uma bola:
 
 instancie duas bolas com dados recebidos do utilizador (input)
 """
+
+class Bola:
+    def __init__(self, cor:str, circunferencia, material):
+        self.cor = cor
+        self.circunferencia = circunferencia
+        self.material = material
+
+    def trocaCor(self, nova_cor):
+        self.cor = nova_cor
+
+    def mostraCor(self):
+        print(self.cor)
+
+
+cor = input("Digite o cor: ")
+circunferencia = float(input("Digite o circunferencia: "))
+material = input("Digite o material: ")
+
+b1 = Bola(cor, circunferencia, material)
+
+cor = input("Digite o cor: ")
+circunferencia = float(input("Digite o circunferencia: "))
+material = input("Digite o material: ")
+
+b2 = Bola("azul", 31, "borracha")
+
+nova_cor = input("Digite a nova cor: ")
+b2.trocaCor(nova_cor)
